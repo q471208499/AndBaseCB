e85658994b077bba0765c43355af9b22829c472a
chenb <471208499@qq.com>
2020-05-13 17:08:57 +0800

    增加日期工具类

--- baselibrary/src/main/java/cn/cb/baselibrary/utils/ABStringUtils.java
+++ baselibrary/src/main/java/cn/cb/baselibrary/utils/ABStringUtils.java
@@ -3,7 +3,7 @@ package cn.cb.baselibrary.utils;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
-public class ABUtils {
+public class ABStringUtils {
     public static boolean isNumeric(String str){
         Pattern pattern = Pattern.compile("[0-9]*");
         Matcher isNum = pattern.matcher(str);
