29b4011e0ba2fd3aa5f85e6be9a46e93beeefdea
chenb <471208499@qq.com>
2020-09-07 12:52:14 +0800

    新增APP更新后半段字符串模板

--- baselibrary/src/main/java/cn/cb/baselibrary/provider/ABConstant.java
+++ baselibrary/src/main/java/cn/cb/baselibrary/provider/ABConstant.java
@@ -3,4 +3,6 @@ package cn.cb.baselibrary.provider;
 public class ABConstant {
     public static final int REQUEST_CODE_SCAN = 1000;
     public static final int REQUEST_CODE_PERMISSION = 1001;
+
+    public static final String APP_UPDATE_SUFFIX = "appUpdate/%1$s/update.txt";
 }
