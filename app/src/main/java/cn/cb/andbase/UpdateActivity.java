f5f8793c3249c97a46b8c094efa36267a6a11869
chenb <471208499@qq.com>
2020-09-07 14:20:52 +0800

    update ui is bug. so use default ui_e

--- app/src/main/java/cn/cb/andbase/UpdateActivity.java
+++ app/src/main/java/cn/cb/andbase/UpdateActivity.java
@@ -10,22 +10,23 @@ import cn.cb.baselibrary.activity.BaseActivity;
 import cn.cb.baselibrary.net.okhttp3.OkHttpException;
 import cn.cb.baselibrary.net.okhttp3.RequestMode;
 import cn.cb.baselibrary.net.okhttp3.ResponseCallback;
+import cn.cb.baselibrary.utils.AppUpdateHelper;
 import es.dmoral.toasty.MyToast;
 
 public class UpdateActivity extends BaseActivity {
     private final String TAG = getClass().getSimpleName();
-    protected String updateInfoUrl = "http://192.168.137.1:8080/proxy/appUpdate/applicationId/update.txt";
+    protected String updateInfoUrl = "http://www.xinchuang-link.cn:8321/appUpdate/cn.hk.easyscribebao/update.txt";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_update);
         initBarView();
-        getUpdateInfo();
+        //getUpdateInfo();
+        new AppUpdateHelper(updateInfoUrl).getUpdateInfo();
     }
 
     private void update(String jsonStr) {
-        //String jsonData = "{\"versionCode\": 25,\"isForceUpdate\": 1,\"preBaselineCode\": 24,\"versionName\": \"v2.3.1\",\"downurl\": \"http://192.168.1.88:8080/MIServer/mobile/app.apk\",\"updateLog\": \"1、优化细节和体验，更加稳定\n2、引入大量优质用户\r\n3、修复已知bug\n4、风格修改\",\"size\": \"31338250\",\"hasAffectCodes\": \"1|2|3|4|5|6|7|8|9|10|11|12|13|14|15|16|17|18|19|20|21|22|23|24\"}";
         AppUpdateUtils.getInstance().addAppDownloadListener(new AppDownloadListener() {
             @Override
             public void downloading(int progress) {
--- baselibrary/src/main/java/cn/cb/baselibrary/BaseApplication.java
+++ baselibrary/src/main/java/cn/cb/baselibrary/BaseApplication.java
@@ -9,7 +9,6 @@ import android.graphics.Bitmap;
 import android.graphics.drawable.BitmapDrawable;
 import android.graphics.drawable.Drawable;
 
-import com.cretin.www.cretinautoupdatelibrary.interfaces.AppDownloadListener;
 import com.cretin.www.cretinautoupdatelibrary.model.TypeConfig;
 import com.cretin.www.cretinautoupdatelibrary.model.UpdateConfig;
 import com.cretin.www.cretinautoupdatelibrary.utils.AppUpdateUtils;
@@ -71,17 +70,14 @@ public class BaseApplication extends Application {
                 .setDataSourceType(TypeConfig.DATA_SOURCE_TYPE_JSON)//设置获取更新信息的方式为JSON方式
                 .setShowNotification(true)//配置更新的过程中是否在通知栏显示进度
                 .setNotificationIconRes(R.mipmap.ic_launcher)//配置通知栏显示的图标
-                .setUiThemeType(TypeConfig.UI_THEME_AUTO)//配置UI的样式，一种有12种样式可供选择
+                //.setUiThemeType(TypeConfig.UI_THEME_AUTO)//配置UI的样式，一种有12种样式可供选择
+                .setUiThemeType(TypeConfig.UI_THEME_E)//配置UI的样式，一种有12种样式可供选择
                 .setAutoDownloadBackground(false)//是否需要后台静默下载，如果设置为true，则调用checkUpdate方法之后会直接下载安装，不会弹出更新页面。当你选择UI样式为TypeConfig.UI_THEME_CUSTOM，静默安装失效，您需要在自定义的Activity中自主实现静默下载，使用这种方式的时候建议setShowNotification(false)，这样基本上用户就会对下载无感知了
                 //.setCustomActivityClass(CustomActivity.class)//如果你选择的UI样式为TypeConfig.UI_THEME_CUSTOM，那么你需要自定义一个Activity继承自RootActivity，并参照demo实现功能，在此处填写自定义Activity的class，否则不用设置
                 .setNeedFileMD5Check(false)//是否需要进行文件的MD5检验，如果开启需要提供文件本身正确的MD5校验码，DEMO中提供了获取文件MD5检验码的工具页面，也提供了加密工具类Md5Utils
                 .setCustomDownloadConnectionCreator(new OkHttp3Connection.Creator(builder))//如果你想使用okhttp作为下载的载体，可以使用如下代码创建一个OkHttpClient，并使用demo中提供的OkHttp3Connection构建一个ConnectionCreator传入，在这里可以配置信任所有的证书，可解决根证书不被信任导致无法下载apk的问题
                 .setModelClass(new UpdateModel());//这里设置JSON解析之后对应的Model 用于json解析
         AppUpdateUtils.init(this, updateConfig);//执行初始化
-
-
-
-
     }
 
     /**
